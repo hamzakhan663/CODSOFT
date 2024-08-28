a78787498e635d09ce69d33200ceb07e32c278ec
hamzakhan663 <sulaimonhamza233@gmail.com>
2024-08-29 00:10:49 +0100

    Task V in progress

---
+++ codsoft_taskno5/contactbook.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Contact Book")
+img = PhotoImage(file = "codsoft_taskno5/icons8-contact-48.png")
+root.iconphoto(False, img)
+
+
+
+
+root.mainloop()
